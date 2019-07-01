bd7480745a5cd9f31c3d892c8829cfd1f31ea1b1
Smith, Irene S. (ARC-TI)[SGT, INC] <irene.s.smith@nasa.gov>
2019-07-01 09:28:24 -0700

    simple outer loop -- one look ahead

--- src/main/java/LookAndSay5.java
+++ src/main/java/LookAndSay5.java
@@ -4,12 +4,12 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * File Name: LookAndSay.java
+ * File Name: LookAndSay.java 
  * LookAndSay concrete class
- *
- *
+ * 
+ * 
  * To Compile: IntUtil.java RandomInt.java LookAndSay.java LookAndSayBase.java
- *
+ * 
  * @author Jagadeesh Vasudevamurthy
  * @year 2019
  */
@@ -19,7 +19,7 @@ class LookAndSay extends LookAndSayBase{
 		//NOTHING CAN BE CHANGED HERE
 		testBench();
 	}
-
+	
 	@Override
 	protected String lookAndSay(String s) {
 		//NOTHING CAN BE CHANGED HERE
@@ -31,66 +31,60 @@ class LookAndSay extends LookAndSayBase{
 		//NOTHING CAN BE CHANGED HERE
 		return alg(n) ;
 	}
-
+	
 	private String alg(String s) {
-		if (s == null) throw new RuntimeException();
-		if (s.isEmpty()) return s;
-
+		if (s == null || s.isEmpty() ) throw new RuntimeException("illegal arg");
+		if (s == "1" || s == "0") return s;
 
 		StringBuilder sb = new StringBuilder();
 		int i = 0;
-		char cur = s.charAt(i);
-		int qty = 1;
-		// start at index 0 and look ahead to next index unless end of string.  End of string is i+1 == length
 		while (i<s.length()) {
-			cur = s.charAt(i);
-
-			if (i+1 == s.length()) {
-				sb.append(Integer.toString(qty)).append(cur);
-				break;
-			}
-			if (cur == s.charAt(i+1)) {
+			int qty = 1;
+			char cur = s.charAt(i);
+			while (i+qty<s.length() && cur == s.charAt(i+qty)) {
 				qty++;
-			} else {
-				sb.append(Integer.toString(qty)).append(cur);
-				qty = 1;
+				i++;
 			}
+			sb.append(Integer.toString(qty)).append(cur);
 			i++;
 		}
+
+
 		return sb.toString();
 
 	}
 
 
-	// given index of look-and-say sequence, return its value
-	//https://en.wikipedia.org/wiki/Look-and-say_sequence
+	// Map of index of look-and-say sequence and the value at that index
+	// https://en.wikipedia.org/wiki/Look-and-say_sequence
 	private Map<Integer, String> dp = new HashMap<>( );
 
-	private Integer maxCache = null;
 
+	private Integer maxCachedIndex = 1;
 
-	// input range, 0 to INTEGER MAX
 	private String alg(int n) {
 		long start = System.currentTimeMillis();
-		if (n ==  0 || n == 1) {
-			dp.put(n,"1");
-			return "1";
-		}
+		if (dp.get(1) == null) dp.put(1,"1");
 
 		//value is available in the cache
 		if (dp.get(n) != null) {
 			return dp.get(n);
 		}
 
-		// calc value from maxCache to n
+        // calc value from 1 to n not using cache
 		String cur="1";
 		for (int i=1 ;i<n;i++) {
-
-			cur = alg(cur);
-			u.myassert(cur != null);
-			dp.put(i, cur);
-
+				cur = alg(cur);
 		}
+
+		// calc value from maxCache to n
+//		String cur = dp.get(maxCachedIndex);
+//		for (int i=maxCachedIndex;i<n;i++) {
+//			cur = alg(cur);
+//			dp.put(i, cur);
+//			maxCachedIndex = i;
+//		}
+
 		System.out.println(String.format("n:%d time:%s", n, System.currentTimeMillis() - start));
 		return cur;// dp.get(n);
 	}
